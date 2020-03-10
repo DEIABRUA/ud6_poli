f079b627cbc7928471e5c8c87e5af9740d57a0a5
Ondero <Ondero@DESKTOP-163FUDE>
2020-03-10 13:05:47 +0100

    Bat

---
+++ src/Controlador/VerUsuario.java
@@ -0,0 +1,52 @@
+package Controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.bean.Usuario;
+import modelo.dao.ModeloUsuario;
+
+/**
+ * Servlet implementation class VerUsuarios
+ */
+@WebServlet("/VerUsuarios")
+public class VerUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public VerUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idUsuario = Integer.parseInt(request.getParameter("id"));
+		
+		ModeloUsuario mU = new ModeloUsuario();
+		ArrayList<Usuario> usuario = mU.selectAll();
+		
+		request.setAttribute("usuario", usuario);
+		
+		request.getRequestDispatcher("VerUsuario.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
